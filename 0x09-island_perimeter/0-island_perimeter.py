b1186fa52566225c6875d6af9f9962c11c06319d
SouaOui <soumiaouzat@gmail.com>
2024-09-24 18:03:29 +0100

    Island Perimeter problem

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -7,22 +7,24 @@ island Perimeter Problem solved using DFS and Graphs in python
 
 
 def island_perimeter(grid):
-	"""
-	Function implementation for Island Calculation of Perimeter
-	"""
-	visit = set()
-	def dfs(i, j):
-		if i >= len(grid) or j >= len(grid[0]) or i < 0 or j <0 or grid[i][j] == 0:
-			return 1
-		if (i, j) in visit:
-			return 0
-		visit.add((i, j))
-		perimeter = dfs(i, j + 1)
-		perimeter += dfs(i + 1, j)
-		perimeter += dfs(i-1, j)
-		perimeter += dfs(i, j -1)
-		return perimeter
-	for i in range(len(grid)):
-		for j in range(len(grid[0])):
-			if grid[i][j]:
-				return dfs(i, j)
+    """
+    Function implementation for Island Calculation of Perimeter
+    """
+    visit = set()
+
+    def dfs(i, j):
+        if i >= len(grid) or j >= len(
+                grid[0]) or i < 0 or j < 0 or grid[i][j] == 0:
+            return 1
+        if (i, j) in visit:
+            return 0
+        visit.add((i, j))
+        perimeter = dfs(i, j + 1)
+        perimeter += dfs(i + 1, j)
+        perimeter += dfs(i - 1, j)
+        perimeter += dfs(i, j - 1)
+        return perimeter
+    for i in range(len(grid)):
+        for j in range(len(grid[0])):
+            if grid[i][j]:
+                return dfs(i, j)
