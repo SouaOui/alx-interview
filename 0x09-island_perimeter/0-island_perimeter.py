35f02e37e26309c4ca68aa06ede2f4ebac6d1d18
SouaOui <soumiaouzat@gmail.com>
2024-09-24 18:02:07 +0100

    Island Perimeter problem

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -1,4 +1,15 @@
+#!/usr/bin/python3
+"""
+
+island Perimeter Problem solved using DFS and Graphs in python
+
+"""
+
+
 def island_perimeter(grid):
+	"""
+	Function implementation for Island Calculation of Perimeter
+	"""
 	visit = set()
 	def dfs(i, j):
 		if i >= len(grid) or j >= len(grid[0]) or i < 0 or j <0 or grid[i][j] == 0:
